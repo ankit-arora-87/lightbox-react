9af27b3f805025c00ccbb7d4a47f3a340d8e10e9
Ankit Arora <arora@ieplsg.com>
2020-06-15 00:38:38 +0800

    created lightbox-image-gallery component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LightboxImageGallery from "./components/lightbox-image-gallery/lightbox-image-gallery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.images = [
+      {
+        image: "img_1.jpg",
+        image_wide: "img_1_wide.jpg",
+        id: 1
+      },
+      {
+        image: "img_2.jpg",
+        image_wide: "img_2_wide.jpg",
+        id: 2
+      },
+      {
+        image: "img_3.jpg",
+        image_wide: "img_3_wide.jpg",
+        id: 3
+      },
+      {
+        image: "img_4.jpg",
+        image_wide: "img_4_wide.jpg",
+        id: 4
+      }
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <LightboxImageGallery images={this.images} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/lightbox-image-gallery/lightbox-image-gallery.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "./lightbox-image-gallery.css";
+
+class LightboxImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 1,
+      isModalOpen: false
+    };
+  }
+
+  openModal = n => {
+    this.setState(
+      {
+        isModalOpen: true
+      },
+      () => {
+        this.currentSlide(n);
+      }
+    );
+  };
+
+  currentSlide = n => {
+    this.setState({
+      slideIndex: n
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false
+    });
+  };
+
+  plusSlides = n => {
+    this.setState(prevState => {
+      return {
+        slideIndex:
+          prevState.slideIndex + n > this.props.images.length
+            ? 1
+            : prevState.slideIndex + n === 0
+            ? this.props.images.length
+            : prevState.slideIndex + n
+      };
+    });
+  };
+  render() {
+    const { images } = this.props;
+    return (
+      <div>
+        <div className="row">
+          {images.map(image => (
+            <div className="column" key={image.id}>
+              <img
+                src={`/images/${image.image}`}
+                onClick={() => this.openModal(image.id)}
+                className="hover-shadow"
+                alt={`small ${image.image}`}
+              />
+            </div>
+          ))}
+        </div>
+
+        <div
+          id="myModal"
+          className="modal"
+          style={
+            this.state.isModalOpen === true
+              ? { display: "block" }
+              : { display: "none" }
+          }
+        >
+          <span className="close cursor" onClick={this.closeModal}>
+            &times;
+          </span>
+          <div className="modal-content">
+            {images.map(image => (
+              <div
+                className="slider"
+                key={`wide_` + image.id}
+                style={
+                  this.state.slideIndex === image.id
+                    ? { display: "block" }
+                    : { display: "none" }
+                }
+              >
+                <div className="pager">
+                  {image.id} / {images.length}
+                </div>
+                <img
+                  src={`/images/${image.image_wide}`}
+                  style={{ width: "100%" }}
+                  alt={`${image.image_wide}`}
+                />
+              </div>
+            ))}
+
+            <span className="prev" onClick={() => this.plusSlides(-1)}>
+              &#10094;
+            </span>
+            <span className="next" onClick={() => this.plusSlides(1)}>
+              &#10095;
+            </span>
+
+            <div className="caption-container">
+              <p id="caption"></p>
+            </div>
+            {images.map(image => (
+              <div className="column" key={image.id}>
+                <img
+                  src={`/images/${image.image}`}
+                  onClick={() => this.currentSlide(image.id)}
+                  className={
+                    `"thumbnail"` + (this.state.slideIndex === image.id)
+                      ? " active"
+                      : ""
+                  }
+                  alt={`thumbnail ${image.image}`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default LightboxImageGallery;
