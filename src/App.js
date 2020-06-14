c224620040655c8fba343295dc795a47fda387c5
Ankit Arora <arora@ieplsg.com>
2020-06-15 00:49:47 +0800

    created assets folder for providing images data list to be rendered in lightbox

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React from "react";
 import LightboxImageGallery from "./components/lightbox-image-gallery/lightbox-image-gallery";
+import IMAGES_DATA from "./assets/images.js";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.images = [
-      {
-        image: "img_1.jpg",
-        image_wide: "img_1_wide.jpg",
-        id: 1
-      },
-      {
-        image: "img_2.jpg",
-        image_wide: "img_2_wide.jpg",
-        id: 2
-      },
-      {
-        image: "img_3.jpg",
-        image_wide: "img_3_wide.jpg",
-        id: 3
-      },
-      {
-        image: "img_4.jpg",
-        image_wide: "img_4_wide.jpg",
-        id: 4
-      }
-    ];
+    this.images = IMAGES_DATA;
   }
 
   render() {
---
+++ src/assets/images.js
@@ -0,0 +1,24 @@
+const IMAGES_DATA = [
+  {
+    image: "img_1.jpg",
+    image_wide: "img_1_wide.jpg",
+    id: 1
+  },
+  {
+    image: "img_2.jpg",
+    image_wide: "img_2_wide.jpg",
+    id: 2
+  },
+  {
+    image: "img_3.jpg",
+    image_wide: "img_3_wide.jpg",
+    id: 3
+  },
+  {
+    image: "img_4.jpg",
+    image_wide: "img_4_wide.jpg",
+    id: 4
+  }
+];
+
+export default IMAGES_DATA;
